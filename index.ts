62672cc4967dfff133d29e7beb507e4c5a65f4df
XBB <cs358118276@qq.com>
2020-02-29 11:51:35 +0800

    decorators & class & extends

--- index.ts
+++ index.ts
@@ -1,52 +1,87 @@
-// TS 中定义函数的方法
-// 函数声明
-// function run(): string {
-//   return "str"
-// }
-// // 匿名函数
-// var run2 = function(): string {
-//   return "str"
-// }
+class Person {
+  // 公有属性
+  public name: string
+  // run = (): void => {}
 
-function getInfo(name: string = "XBB", age?: number): string {
-  if (age) {
-    return `${name} --- ${age}`
-  } else {
-    return `${name} --- ${25}`
+  getName() {
+    console.log(this.name)
+  }
+
+  constructor(name: string) {
+    this.name = name
+  }
+
+  static printInfo() {
+    console.log("static printInfo")
   }
 }
 
-// ?: 指的是可选参数 并且必须要放到参数的最后
-// 可选参数没有默认值
-
-// 剩余参数
-// function sum(a: number, b: number, c: number, d: number): number {
-//   return a + b + c + d
-// }
-// 可以替换成
-function sum(a: number, ...result: number[]): number {
-  return result.reduce((total, value) => {
-    return total + value
-  }, a)
-}
-
-// 函数重载 但与JAVA中的重载有所不同
-// 前两个无函数体
-function get(name: string): string
-
-function get(name: string, age: number): string
-// 汇总 内部判断参数类型
-// str 允许的类型仅仅包括已经定义的 number 和 string
-function get(name: any, age?: any): string {
-  if (age) {
-    return `${name} --- ${age}`
-  } else {
-    return `${name}`
+class MyWorker extends Person {
+  constructor(name: string) {
+    // 调用父类的构造方法
+    super(name)
   }
 }
 
-// console.log(get("XBB", 12))
+// TS 提供了三种类型 属性修饰符
+// public 默认
+// protected 类内 子类内 可访问 外部没法访问
+// private 类内可访问 子类内 外部无法访问
 
-var foo1 = (): number => {
-  return 123
+// var p = new Person("XBB")
+// Person.printInfo()
+// p.getName()
+
+// 类的多态
+// 多个类继承
+// class Animal {
+//   name: string
+//   constructor(name: string) {
+//     this.name = name
+//   }
+//   // 具体 由继承的类实现 不同的子类有不同的表现
+//   eat() {
+//     console.log("吃的方法")
+//   }
+// }
+
+// class Dog extends Animal {
+//   constructor(name: string) {
+//     super(name)
+//   }
+//   eat() {
+//     return this.name + "骨头"
+//   }
+// }
+
+// class Cat extends Animal {
+//   constructor(name: string) {
+//     super(name)
+//   }
+//   eat() {
+//     return this.name + "老鼠"
+//   }
+// }
+
+// 抽象方法只放在抽象类当中
+// 用来定义标准
+abstract class Animal {
+  name: string
+  constructor(name: string) {
+    this.name = name
+  }
+  // 抽象函数 不需要函数体 用于提供标准
+  // 要求继承的非抽象类必须要实现该抽象方法
+  abstract eat(): any
+}
+
+class Dog extends Animal {
+  constructor(name: string) {
+    super(name)
+  }
+  // 抽象类的子类必须要实现抽象类里面的抽象方法
+  // 或继续抽象
+  eat(): string {
+    return "meat"
+  }
 }
--- js/index.js
+++ js/index.js
@@ -1,49 +1,90 @@
 "use strict";
-// TS 中定义函数的方法
-// 函数声明
-// function run(): string {
-//   return "str"
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Person = /** @class */ (function () {
+    function Person(name) {
+        this.name = name;
+    }
+    // run = (): void => {}
+    Person.prototype.getName = function () {
+        console.log(this.name);
+    };
+    Person.printInfo = function () {
+        console.log("static printInfo");
+    };
+    return Person;
+}());
+var MyWorker = /** @class */ (function (_super) {
+    __extends(MyWorker, _super);
+    function MyWorker(name) {
+        // 调用父类的构造方法
+        return _super.call(this, name) || this;
+    }
+    return MyWorker;
+}(Person));
+// TS 提供了三种类型 属性修饰符
+// public 默认
+// protected 类内 子类内 可访问 外部没法访问
+// private 类内可访问 子类内 外部无法访问
+// var p = new Person("XBB")
+// Person.printInfo()
+// p.getName()
+// 类的多态
+// 多个类继承
+// class Animal {
+//   name: string
+//   constructor(name: string) {
+//     this.name = name
+//   }
+//   // 具体 由继承的类实现 不同的子类有不同的表现
+//   eat() {
+//     console.log("吃的方法")
+//   }
 // }
-// // 匿名函数
-// var run2 = function(): string {
-//   return "str"
+// class Dog extends Animal {
+//   constructor(name: string) {
+//     super(name)
+//   }
+//   eat() {
+//     return this.name + "骨头"
+//   }
 // }
-function getInfo(name, age) {
-    if (name === void 0) { name = "XBB"; }
-    if (age) {
-        return name + " --- " + age;
-    }
-    else {
-        return name + " --- " + 25;
-    }
-}
-// ?: 指的是可选参数 并且必须要放到参数的最后
-// 可选参数没有默认值
-// 剩余参数
-// function sum(a: number, b: number, c: number, d: number): number {
-//   return a + b + c + d
+// class Cat extends Animal {
+//   constructor(name: string) {
+//     super(name)
+//   }
+//   eat() {
+//     return this.name + "老鼠"
+//   }
 // }
-// 可以替换成
-function sum(a) {
-    var result = [];
-    for (var _i = 1; _i < arguments.length; _i++) {
-        result[_i - 1] = arguments[_i];
+// 抽象方法只放在抽象类当中
+// 用来定义标准
+var Animal = /** @class */ (function () {
+    function Animal(name) {
+        this.name = name;
     }
-    return result.reduce(function (total, value) {
-        return total + value;
-    }, a);
-}
-// 汇总 内部判断参数类型
-// str 允许的类型仅仅包括已经定义的 number 和 string
-function get(name, age) {
-    if (age) {
-        return name + " --- " + age;
+    return Animal;
+}());
+var Dog = /** @class */ (function (_super) {
+    __extends(Dog, _super);
+    function Dog(name) {
+        return _super.call(this, name) || this;
     }
-    else {
-        return "" + name;
-    }
-}
-// console.log(get("XBB", 12))
-var foo1 = function () {
-    return 123;
-};
+    // 抽象类的子类必须要实现抽象类里面的抽象方法
+    // 或继续抽象
+    Dog.prototype.eat = function () {
+        return "meat";
+    };
+    return Dog;
+}(Animal));
